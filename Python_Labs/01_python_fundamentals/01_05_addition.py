1f672d5e79edf6723c6d5365fd261568fac4b6a6
ben91chen <bc9938@gmail.com>
2021-02-13 19:50:50 -0700

    Modified lab

--- Python_Labs/01_python_fundamentals/01_05_addition.py
+++ Python_Labs/01_python_fundamentals/01_05_addition.py
@@ -5,4 +5,8 @@ Write the necessary code to print out the result of the following:
 
 '''
 
-print(2 + 4 + 6 + 8 + 9 + 10 + 12 + 14 + 16 + 18)
+#print(2 + 4 + 6 + 8 + 9 + 10 + 12 + 14 + 16 + 18)
+print(3/2)
+print((1/1))
+print(5/4)
+print(2/2)
