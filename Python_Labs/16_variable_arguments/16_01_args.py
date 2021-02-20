5efaeedce2211c675eb2d79fe6dc32914034e244
ben91chen <bc9938@gmail.com>
2021-02-20 14:05:45 -0700

    Committing labs from module 16

--- Python_Labs/16_variable_arguments/16_01_args.py
+++ Python_Labs/16_variable_arguments/16_01_args.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def to_do_list(*args):
+    for a in args:
+        print(a)
+
+to_do_list("clean house", "feed dog", "make dinner", "go for run")
--- Python_Labs/16_variable_arguments/16_02_kwargs.py
+++ Python_Labs/16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def age(**kwargs):
+    for k,v in kwargs.items():
+        print(f"{k} is {v} years old.")
+
+age(Ben = 30, Bob = 35, John = 28)
+
