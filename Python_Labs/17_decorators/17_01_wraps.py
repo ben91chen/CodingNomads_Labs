867f29a30fe1dfa6cc5d835d93040980573244a3
ben91chen <bc9938@gmail.com>
2021-03-04 21:09:22 -0700

    Committing labs from 17_decorators

--- Python_Labs/17_decorators/17_01_wraps.py
+++ Python_Labs/17_decorators/17_01_wraps.py
@@ -2,3 +2,16 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+def p_tag_wrapper(f):
+    def decorator(text):
+        text = f"<p>{f(text)}</p>"
+        return text
+    return decorator
+
+@p_tag_wrapper
+def sentence(text):
+    return text
+
+print(sentence("hello"))
+
--- Python_Labs/17_decorators/17_02_tags.py
+++ Python_Labs/17_decorators/17_02_tags.py
@@ -3,3 +3,19 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+def tag_wrapper(tag):
+    def html_tag(f):
+        def decorator(text):
+            text = f"<{tag}>{f(text)}</{tag}>"
+            return text
+        return decorator
+    return html_tag
+
+# @tag_wrapper('p')
+# @tag_wrapper('b')
+def sentence(text):
+    return text
+
+sentence = tag_wrapper('b')(sentence)
+print(sentence("hello"))
